3bc63c7fb25d50f648d9c45835521a565319eb9e
ANDRY Nirina <ani@esokia.com>
2020-04-26 11:53:14 +0300

    minify

--- js/home.js
+++ js/home.js
@@ -1,13 +1,1 @@
-var play_list_string
-var curent_play
-$(document).ready(function(){
-    refreshPlayLabel();
-})
-
-function refreshPlayLabel(){
-    setTimeout(function(){ 
-        play_list_string = chrome.extension.getBackgroundPage().play_list_string;
-        curent_play = chrome.extension.getBackgroundPage().player.getPlaylistIndex();
-        $(".curent_play_label").text(play_list_string[curent_play]);
-    }, 2000);
-}
+function refreshPlayLabel(){setTimeout(function(){play_list_string=chrome.extension.getBackgroundPage().play_list_string,curent_play=chrome.extension.getBackgroundPage().player.getPlaylistIndex(),$(".curent_play_label").text(play_list_string[curent_play])},2e3)}var play_list_string,curent_play;$(document).ready(function(){refreshPlayLabel()});
